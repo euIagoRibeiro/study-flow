39e14181b8dfa43cc1850099494452f7a8086e2f
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2026-02-28 23:56:13 -0300

    feat: basic dynamic list creation with javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,42 @@
-    const topicInput = document.getElementById('topic-input');
-    const difficultySelect = document.getElementById('difficulty-select');
+
     const studyForm = document.getElementById('study-form');
     const studiesList = document.getElementById('studies-list');
 
-
-    studyForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        console.log(`Topic: ${topicInput.value} | Difficulty: ${difficultySelect.value}`);
+    function createStudyItem(topic, difficulty) {
 
         const li = document.createElement('li');
-        li.textContent = `Topic: ${topicInput.value} | Difficulty: ${difficultySelect.value}`;
-        studiesList.appendChild(li);
+        li.classList.add('study-item');
+
+        const studyInfoDiv = document.createElement('div');
+        studyInfoDiv.classList.add('study-info');
+
+        const topicSpan = document.createElement('span');
+        topicSpan.classList.add('topic-name');
+        topicSpan.textContent = topic;
+        studyInfoDiv.appendChild(topicSpan);
+
+        const difficultySpan = document.createElement('span');
+        difficultySpan.classList.add('difficulty-tag');
+        difficultySpan.classList.add(`tag-${difficulty}`);
+        difficultySpan.textContent = difficulty;
+
+        li.appendChild(studyInfoDiv);
+        li.appendChild(difficultySpan);
+        
+        return li;
+    }
+
+
+    studyForm.addEventListener('submit', (event) => {
+
+        event.preventDefault();
+
+        const topicInput = document.getElementById('topic-input');
+        const difficultySelect = document.getElementById('difficulty-select');
+
+        const newEntry = createStudyItem(topicInput.value, difficultySelect.value);
+        studiesList.appendChild(newEntry);
+        
         topicInput.value = '';
         topicInput.focus();
 
