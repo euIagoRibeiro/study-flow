7d21546843f1a1b6f697ee7da6b920d7262a3a72
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2026-09-11 23:26:15 -0300

    feat: create Task component

---
+++ src/components/Task.tsx
@@ -0,0 +1,10 @@
+
+function Task(props: {title: string; completed?: boolean}) {
+    
+    return (
+        <p>Tarefa: {props.title} | {props.completed ? "ok" : "não"}</p>
+    )
+
+}
+
+export default Task;
